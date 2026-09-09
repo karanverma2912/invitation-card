88e8b7c1fd1add0c2366bd46771ccdf213023ba5
Karan Verma <karanv@shriffle.com>
2026-09-09 22:48:38 +0530

    Initial commit from Create Next App

---
+++ src/data/wedding.ts
@@ -0,0 +1,60 @@
+export const wedding = {
+  groom: {
+    name: "Karan Verma",
+    hometown: "Maheshwar",
+    photo: "/images/couple/karan.jpg",
+  },
+  bride: {
+    name: "Damini Shivde",
+    hometown: "Omkareshwar",
+    photo: "/images/couple/damini.jpg",
+  },
+  weddingDate: "11 February",
+  reception: {
+    date: "12 February",
+    location: "Maheshwar",
+  },
+  events: [
+    {
+      date: "9 February",
+      title: "Khatu Shyam Bhajan",
+      time: "Evening",
+    },
+    {
+      date: "9 February",
+      title: "Mehndi",
+      time: "Evening",
+    },
+    {
+      date: "9 February",
+      title: "Khal Maati",
+      time: "Evening",
+    },
+    {
+      date: "10 February",
+      title: "Haldi",
+      time: "Morning",
+    },
+    {
+      date: "11 February",
+      title: "Sagai",
+      time: "Morning / Afternoon",
+    },
+    {
+      date: "11 February",
+      title: "Lagan",
+      time: "Evening",
+    },
+    {
+      date: "11 February",
+      title: "Wedding",
+      time: "Evening",
+    },
+    {
+      date: "12 February",
+      title: "Reception",
+      location: "Maheshwar",
+      time: "Evening",
+    },
+  ],
+};
